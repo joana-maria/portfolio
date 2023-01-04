06effb47c69e35cd6da9bfc1835ef12ffdffa1c9
joana-maria <joanamcoliv@gmail.com>
2023-01-04 15:22:08 +0000

    Manually retrive JSON

--- src/composables/getWork.js
+++ src/composables/getWork.js
@@ -1,28 +1,53 @@
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const getWork = (id) =>{
-    const work = ref(0)
-    const error = ref(null)
-    const names = ["","creative-emotions","phototyper","a-small-showcase-of-graphic-design-and-world-history","cuidar-de-ti","the-queens-gambit-project","nei","develop-way-you-think-as-a-designer","grokem-rehany-zejuh"];
+const getWork = (id) => {
+  const work = ref(0);
+  const error = ref(null);
+  const names = [
+    "",
+    "creative-emotions",
+    "phototyper",
+    "a-small-showcase-of-graphic-design-and-world-history",
+    "cuidar-de-ti",
+    "the-queens-gambit-project",
+    "nei",
+    "develop-way-you-think-as-a-designer",
+    "grokem-rehany-zejuh",
+  ];
 
-    const load = async () => {
-        try{
-         let data = await fetch('http://localhost:3000/works/' + id)
-          
-          if(!data.ok){
-             throw Error('That post does not exist!')
+  const load = async () => {
+    try {
+      //  let data = await fetch('http://localhost:3000/works/' + id)
+
+      //   if(!data.ok){
+      //      throw Error('That post does not exist!')
+      //   }
+
+      //   work.value = await data.json();
+      //   console.log(work.value)
+
+      fetch("/data/works.json")
+        .then((res) => res.json())
+        .then((data2) => {
+          console.log("locally: ");
+          console.log(data2);
+         // works.value = data2.works;
+
+         data2.works.forEach(workClicked => {
+          if(workClicked.id === id){
+            console.log("work ")
+            console.log(workClicked)
+            work.value = workClicked;
           }
-
-          work.value = await data.json();
-        }
-        catch (err){
-          error.value = err.message
-          console.log(error.value)
-        }
+         });
+        });
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
     }
+  };
 
-    return {work, error, load}
-
-}
+  return { work, error, load };
+};
 
 export default getWork;
--- src/composables/getWorks.js
+++ src/composables/getWorks.js
@@ -1,28 +1,36 @@
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const getWorks = () => {
-    const works = ref([])
-    const error = ref(null)
+  const works = ref([]);
+  const error = ref(null);
 
-    const load = async () => {
-        try{
-          let data = await fetch('http://localhost:3000/works')
-          //console.log(data)
+  const load = async () => {
+    try {
+     //   let data = await fetch("http://localhost:3000/works");
 
-          if(!data.ok){
-             throw Error('no data available')
-          }
+      // if (!data.ok) {
+      //   throw Error("no data available");
+      // }
 
-          works.value = await data.json();
-        }
-        catch (err){
-          error.value = err.message
-          console.log(error.value)
-        }
+   //   works.value = await data.json();
+
+      fetch("/data/works.json")
+        .then((res) => res.json())
+        .then((data2) => {
+          console.log("locally: ");
+          console.log(data2);
+          works.value =data2.works;
+          console.log(data2.works);
+        });
+
+     
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
     }
+  };
 
-    return {works, error, load}
+  return { works, error, load };
+};
 
-}
-
-export default getWorks
+export default getWorks;
